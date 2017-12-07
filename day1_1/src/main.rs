e2bf5d000f2be3f0ec813682bcf3c30b71a76002
Hugo Leblanc <dullin@gmail.com>
2017-12-07 14:20:18 -0500

    Added unit tests

--- day1_1/src/main.rs
+++ day1_1/src/main.rs
@@ -12,6 +12,11 @@ fn main() {
                         .map(|c| c.to_digit(10).unwrap())
                         .collect();
 
+    println!("{}", sum_next_array(num_array));
+}
+
+fn sum_next_array(num_array: Vec<u32>) -> u32{
+
     //Check for consequent digits
     //Note : last returns an address
     let mut last = *num_array.last().unwrap();
@@ -22,6 +27,35 @@ fn main() {
         }
         last = i;
     }
+    sum
+}
 
-    println!("{}", sum);
+
+#[cfg(test)]
+mod tests {
+    use sum_next_array;
+
+    #[test]
+    fn test_1() {
+        let num_vec = vec![1, 1, 2, 2];
+        assert_eq!(sum_next_array(num_vec), 3);
+    }
+
+    #[test]
+    fn test_2() {
+        let num_vec = vec![1, 1, 1, 1];
+        assert_eq!(sum_next_array(num_vec), 4);
+    }
+
+    #[test]
+    fn test_3() {
+        let num_vec = vec![1, 2, 3, 4];
+        assert_eq!(sum_next_array(num_vec), 0);
+    }
+
+    #[test]
+    fn test_4() {
+        let num_vec = vec![9,1,2,1,2,1,2,9];
+        assert_eq!(sum_next_array(num_vec), 9);
+    }
 }
