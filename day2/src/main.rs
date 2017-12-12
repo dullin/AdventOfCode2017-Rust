8a558fe94aab96e1594af863f49efbdbbd1323bb
Hugo Leblanc <dullin@gmail.com>
2017-12-11 21:36:29 -0500

    Added day 2 part 2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,8 +4,10 @@ fn main() {
 
     let num_array = parse(&contents);
 
-    let checksumPart1: i32 = num_array.iter().map(line_diff).sum();
-    println!("Part 1 : {}", checksumPart1);
+    let checksum_part1: i32 = num_array.iter().map(line_diff).sum();
+    let checksum_part2: i32 = num_array.iter().map(line_even_divide_sum).sum();
+    println!("Part 1 : {}", checksum_part1);
+    println!("Part 2 : {}", checksum_part2);
 }
 
 fn parse(s: &str) -> Vec<Vec<i32>> {
@@ -17,33 +19,45 @@ fn parse_line(line: &str) -> Vec<i32> {
 }
 
 fn line_diff(line: &Vec<i32>) -> i32 {
-    (*line.iter().max().unwrap() - *line.iter().min().unwrap()).abs()
+    (line.iter().max().unwrap() - line.iter().min().unwrap()).abs()
 }
 
+//TODO Refactor ugly for loops
 fn line_even_divide_sum(line: &Vec<i32>) -> i32 {
     for value1 in line {
-        even_div = 0;
         for value2 in line {
-            if value1 != value2 & value1 % value2 == 0 {
-                
+            if (value1 != value2) & (value1 % value2 == 0) {
+                return value1 / value2
             }
         }
     }
+    panic!("Didn't find even values in line")
 }
 
 #[cfg(test)]
 mod tests {
     use parse;
     use line_diff;
+    use line_even_divide_sum;
 
     #[test]
     fn test_p1() {
-        let contents = include_str!("../datatest.txt");
+        let contents = include_str!("../datatest1.txt");
 
         let num_array = parse(&contents);
 
         let checksum: i32 = num_array.iter().map(line_diff).sum();
         assert_eq!(checksum, 18);
+    }
+
+    #[test]
+    fn test_p2() {
+        let contents = include_str!("../datatest2.txt");
+
+        let num_array = parse(&contents);
+
+        let checksum: i32 = num_array.iter().map(line_even_divide_sum).sum();
+        assert_eq!(checksum, 9);
     }
 
 }
